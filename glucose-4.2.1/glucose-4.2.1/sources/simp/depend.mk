c88948d7323b437dedab6152644e0bd103340799
Yerimar <yeritachan@gmail.com>
2024-03-21 00:50:42 -0400

    Glucose y avance de Actividad 2 (base de ICalendar)

---
+++ glucose-4.2.1/glucose-4.2.1/sources/simp/depend.mk
@@ -0,0 +1,42 @@
+/home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/Main.o /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/Main.or /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/Main.od /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/Main.op: \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/Main.cc \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/System.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/Options.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/Dimacs.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Map.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/Solver.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/Constants.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/SolverStats.h
+/home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/SimpSolver.o /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/SimpSolver.or /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/SimpSolver.od /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/SimpSolver.op: \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/SimpSolver.cc \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Sort.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/Solver.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/Options.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Map.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/Constants.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../core/SolverStats.h \
+ /home/yerimar/-proyecto-3-ci5437/glucose-4.2.1/glucose-4.2.1/sources/simp/../utils/System.h
